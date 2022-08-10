2f5c1c93b1c9505e72920b4c644d3537c5c9af7a
Amirul Islam <amirulandalib9@gmail.com>
2022-08-10 21:43:52 +0600

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -92,9 +92,7 @@ echo "::endgroup::"
 
 echo "::group::Removing NodeJS, NPM & NPX"
 {
-  sudo npm list -g --depth=0. 2>/dev/null | awk -F ' ' '{print $2}' | awk -F '@[0-9]' '{print $1}' | grep -v "^n$" | sudo xargs npm remove -g
-  yes | sudo n uninstall
-  parallel --use-cpus-instead-of-cores sudo rm -rf {} 2>/dev/null ::: /usr/local/lib/node_modules ::: /usr/local/n ::: /usr/local/bin/n /usr/local/bin/vercel /usr/local/bin/now
+  parallel --use-cpus-instead-of-cores sudo rm -rf {} 2>/dev/null ::: /usr/local/bin/vercel /usr/local/bin/now
 } &>/dev/null
 echo "::endgroup::"
 
