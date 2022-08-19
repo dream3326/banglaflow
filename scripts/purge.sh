f03a860aa4eaf432293c31ce7317c11618c71ec5
Amirul Islam <amirulandalib9@gmail.com>
2022-08-19 18:12:36 +0600

    Create purge.sh

---
+++ scripts/purge.sh
@@ -0,0 +1,5 @@
+# Requires GitHub CLI installed and authenticated.
+
+export BRANCH="master"
+
+gh api repos/$GH_USR/$REPOP/actions/runs | jq -r '.workflow_runs[] | select(.head_branch == "master") | "\(.id)"' | xargs -n1 -I % gh api repos/$GH_USR/$REPOP/actions/runs/% -X DELETE > out.log 2> /dev/null
