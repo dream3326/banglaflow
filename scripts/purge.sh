0c2454752d243ffd4b2e93379065dcd5c1f13670
Amirul Islam <amirulandalib9@gmail.com>
2022-08-19 18:16:41 +0600

    Update purge.sh

--- scripts/purge.sh
+++ scripts/purge.sh
@@ -2,4 +2,6 @@
 
 export BRANCH="master"
 
+gh auth login --with-token $GH_TOKEN
+
 gh api repos/$GH_USR/$REPOP/actions/runs | jq -r '.workflow_runs[] | select(.head_branch == "master") | "\(.id)"' | xargs -n1 -I % gh api repos/$GH_USR/$REPOP/actions/runs/% -X DELETE > out.log 2> /dev/null
